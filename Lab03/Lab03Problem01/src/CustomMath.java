01322def232d33d00d0170c398f62f0fb87d0b58
Isakov Sultanbek Aibekovich <isakov_s@auca.kg>
2019-03-14 21:19:20 +0600

    Commit

--- Lab03/Lab03Problem01/src/CustomMath.java
+++ Lab03/Lab03Problem01/src/CustomMath.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class CustomMath {
+
+    //GCD 1
+    public static int gcd(int a, int b) {
+        if (a == 0 && b == 0) {
+            System.out.println("GCD(0, 0) is not defined");
+        } else if (a == 0) {
+            return b;
+        } else if (b == 0) {
+            return a;
+        }
+        int d = Math.min(a, b);
+        while (a % d != 0 || b % d != 0) {
+            --d;
+        }
+        return d;
+    }
+    private CustomMath(){
+
+    }
+    //GCD
+    static int gcd2(int a, int b) {
+        return b == 0 ? a : gcd2(b, a % b);
+    }
 }
--- Lab03/Lab03Problem01/src/Rational.java
+++ Lab03/Lab03Problem01/src/Rational.java
@@ -1,4 +1,96 @@
-package PACKAGE_NAME;
-
 public class Rational {
+    // State
+    private int numerator;
+    private int denominator;
+
+
+    //Behaviour
+    Rational(int numerator, int denominator) {
+        if (denominator == 0) {
+            throw new ArithmeticException("Incorrect rational: Denominator can't be zero");
+        }
+        this.numerator = numerator;
+        this.denominator = denominator;
+
+        int gcd = CustomMath.gcd2(numerator,denominator);
+        this.numerator /= gcd;
+        this.denominator /= gcd;
+        //TODO: simplify (gcd)
+    }
+
+    //Parse
+    static Rational parse(String s) {
+        String[] parts = s.split("/");
+        if (parts.length == 0 || parts.length > 2){
+            throw new IllegalArgumentException("Incorrect rational:  " + s);
+        }
+        try {
+            if (parts.length == 1) {
+                return new Rational(Integer.parseInt(parts[0]), 1);
+            } else {
+                return new Rational(Integer.parseInt(parts[0]),Integer.parseInt(parts[1]));
+            }
+        }catch (NumberFormatException e){
+            throw new NumberFormatException("Incorrect rational: " + s);
+        }
+
+//        int index = s.indexOf('/');
+//        if (index == -1) {
+//            int tnum = Integer.parseInt(s);
+//            return new Rational(tnum, 1);
+//        }
+//        int tnum = Integer.parseInt(s.substring(0, index));
+//        int tden = Integer.parseInt(s.substring(index + 1));
+//        return new Rational(tnum, tden);
+    }
+
+
+    // Compare Rationals
+    public int compareTo(Rational other) {
+        int a = numerator * other.denominator;
+        int b = denominator * other.numerator;
+
+        return a - b;
+    }
+
+    //toString
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+
+    Rational add(Rational other) {
+        int resN = numerator * other.denominator + denominator * other.numerator;
+        int resD = denominator * other.denominator;
+        Rational result = new Rational(resN, resD);
+
+        return result;
+    }
+
+    Rational sub(Rational other) {
+
+        int resN = numerator * other.denominator - denominator * other.numerator;
+        int resD = denominator * other.denominator;
+        Rational result = new Rational(resN, resD);
+
+        return result;
+    }
+
+    Rational mult(Rational other) {
+
+        int resN = numerator * other.numerator;
+        int resD = denominator * other.denominator;
+        Rational result = new Rational(resN, resD);
+
+        return result;
+    }
+
+    Rational div(Rational other) {
+
+        int resN = numerator * other.denominator;
+        int resD = denominator * other.numerator;
+        Rational result = new Rational(resN, resD);
+
+        return result;
+    }
 }
